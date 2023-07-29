49d8d213a68cf787e3e7f468eef2a854ad6bc922
TizzySaurus <tizzysaurus@gmail.com>
2023-07-29 14:15:18 +0100

    Add code for parsing buildings.json config file

--- src/app/constants/buildings.ts
+++ src/app/constants/buildings.ts
@@ -1,37 +1,300 @@
-import constructionCategories from '../../assets/config/constructionCategories.json' assert { type: 'json' };
+import buildings from "../../assets/config/buildings.json" assert { type: "json" };
+import constructionCategories from "../../assets/config/constructionCategories.json" assert { type: "json" };
 
-const hasKey = <o, k extends string>(
-    o: o,
-    k: k
-): o is Extract<o, { [_ in k]: {} }> => {
-    const valueAtKey = (o as any)?.[k]
-    return valueAtKey !== undefined && valueAtKey !== null
-}
+const hasKey = <o, k extends string>(o: o, k: k): o is Extract<o, { [_ in k]: {} }> => {
+    const valueAtKey = (o as any)?.[k];
+    return valueAtKey !== undefined && valueAtKey !== null;
+};
+
+type evaluate<t> = { [k in keyof t]: t[k] } & unknown;
+export type List<t = unknown> = readonly t[];
+export type entryOf<o> = evaluate<
+    { [k in keyof o]-?: [k, Exclude<o[k], undefined>] }[o extends List ? keyof o & number : keyof o]
+>;
+export type entriesOf<o extends object> = entryOf<o>[];
+export const entriesOf = <o extends object>(o: o) => Object.entries(o) as entriesOf<o>;
 
 type Category = {
-    categoryName: string,
-    buttonsType: string,
-    children: (Category | {itemName: string})[]
-}
+    categoryName: string;
+    buttonsType: string;
+    children: (Category | { itemName: string })[];
+};
 
-type ParsedCategoryValue = string | Record<Category["categoryName"], string[]>
+type ParsedCategoryValue = string | Record<Category["categoryName"], string[]>;
 type ParsedCategory = Record<Category["categoryName"], ParsedCategoryValue[]>;
 
 const parsedConstructionCategories: ParsedCategory = {};
 function recurseConstructionCategory(category: Category) {
-    const categoryItems: ParsedCategoryValue[] = []
+    const categoryItems: ParsedCategoryValue[] = [];
     for (const child of category.children) {
         if (hasKey(child, "categoryName")) {
             // Is a sub-category
-            categoryItems.push({[child.categoryName]: recurseConstructionCategory(child) as string[]})
+            categoryItems.push({
+                [child.categoryName]: recurseConstructionCategory(child) as string[],
+            });
         } else {
             categoryItems.push(child.itemName);
         }
     }
-    return categoryItems
+    return categoryItems;
 }
 
 for (const rootCategory of constructionCategories["categories"]) {
-    parsedConstructionCategories[rootCategory.categoryName] = recurseConstructionCategory(rootCategory);
+    parsedConstructionCategories[rootCategory.categoryName] =
+        recurseConstructionCategory(rootCategory);
 }
 console.log(JSON.stringify(parsedConstructionCategories, null, 2));
+
+type ResourceQuantity = {
+    resourceName: string;
+    amount: number;
+};
+
+type ThreeDimensionalCoord = {
+    // NB: Can have negatives (e.g for netEdges)
+    x: number;
+    y: number;
+    z: number;
+};
+
+type BoxCollider = { type: "box"; center: ThreeDimensionalCoord; size: ThreeDimensionalCoord };
+type CapsuleCollider = {
+    type: "capsule";
+    center: ThreeDimensionalCoord;
+    radius: number;
+    height: number;
+};
+type Collider = BoxCollider | CapsuleCollider;
+
+type ProductionDefinition = {
+    consumables: ResourceQuantity[] | string[]; // string[] for e.g. storehouse
+    producables: ResourceQuantity[];
+    timeSteps: number;
+    powerNeeded?: number;
+    maxWorkers: number;
+    yieldResourceName?: string;
+    hasResourceRotation?: boolean;
+};
+
+type BaseProductionLogic = {
+    type: string;
+    productionDefinition?: ProductionDefinition;
+    powerNeeded?: number;
+    nextLevelBuilding?: keyof Buildings;
+};
+
+type HabitatProductionLogic = {
+    type: "habitat";
+    habitatLevel: number;
+    downgrade: string;
+    maxInhabitants: 52;
+    powerNeededForTenPeople: number;
+};
+
+type Edge = {
+    type: string;
+    name: string;
+    startPos: ThreeDimensionalCoord;
+    endPos: ThreeDimensionalCoord;
+    trafficType: string;
+};
+
+type BuildingConnectorPoints = {
+    powerLine: ThreeDimensionalCoord[];
+};
+
+type Building = typeof buildings[BuildingName];
+// type Building = {
+//     costs?: ResourceQuantity[];
+//     localizationOverride?: string;
+//     buildingSystem?: string;
+//     requiredTech?: string[];
+//     buildingSound?: string;
+//     workingSound?: string;
+//     shrinkColliders?: boolean;
+//     colliders: Collider[];
+//     modelVariations?: number;
+//     productionLogic?: BaseProductionLogic | HabitatProductionLogic;
+//     buildingConnectorPoints?: BuildingConnectorPoints;
+//     windEfficiencyRadius?: number;
+//     netEdges?: Edge[];
+//     sideRoadOffset?: ThreeDimensionalCoord;
+//     pipeResourceNames?: string[];
+//     isGondolaStation?: boolean;
+//     distanceCostMultiplier?: number;
+//     placesBuildingConnector?: string;
+//     inBetweenObject?: string;
+//     stationModule?: string;
+//     doesAlwaysWork?: boolean;
+//     interactable?: boolean;
+//     doesAffectTerrain?: boolean;
+//     hasRoadConnection?: boolean;
+//     buildingTiltRatio?: number;
+// };
+
+type ParsedBuildingItemQuantity = Record<string, number>;
+type ParsedBuilding = {
+    name: string;
+    buildCost: ParsedBuildingItemQuantity;
+    categoryPath: string[] | null;
+    duration: number;
+    input: ParsedBuildingItemQuantity;
+    output: ParsedBuildingItemQuantity;
+    power: number;
+    workers: number;
+};
+
+type BuildingName = keyof typeof buildings;
+type Buildings = {
+    [buildingName in BuildingName]: Building;
+};
+
+function getCategoryPath(categories: ParsedCategory, target: string): string[] | null {
+    for (const [categoryName, categoryChildren] of Object.entries(categories)) {
+        for (const child of categoryChildren) {
+            if (typeof child === "string") {
+                if (child === target) {
+                    return [categoryName];
+                }
+                continue;
+            }
+
+            let path = getCategoryPath(child, target);
+            if (path !== null) {
+                return [categoryName, ...path];
+            }
+        }
+    }
+    return null;
+}
+
+function rawCostsToParsedCosts(rawBuilding: Building): ParsedBuilding["buildCost"] {
+    if (!hasKey(rawBuilding, "costs")) return {};
+
+    const parsedCostsForBuilding: ParsedBuilding["buildCost"] = {};
+    for (const cost of rawBuilding.costs) {
+        parsedCostsForBuilding[cost["resourceName"]] = cost["amount"];
+    }
+    return parsedCostsForBuilding;
+}
+
+function getMaxWorkersFromRawBuilding(rawBuilding: Building) {
+    if (!hasKey(rawBuilding, "productionLogic")) return 0;
+
+    if (hasKey(rawBuilding.productionLogic, "productionDefinition"))
+        return rawBuilding.productionLogic.productionDefinition.maxWorkers;
+
+    if (hasKey(rawBuilding.productionLogic, "maxInhabitants"))
+        return rawBuilding.productionLogic.maxInhabitants;
+    return 0;
+}
+
+function getConsumablesOfRawBuilding(rawBuilding: Building) {
+    if (
+        !hasKey(rawBuilding, "productionLogic") ||
+        !hasKey(rawBuilding.productionLogic, "productionDefinition")
+    )
+        return [];
+    return rawBuilding.productionLogic.productionDefinition.consumables;
+}
+
+function getPowerFromRawBuilding(rawBuilding: Building) {
+    if (!hasKey(rawBuilding, "productionLogic")) return 0;
+
+    if (hasKey(rawBuilding.productionLogic, "powerNeeded"))
+        return rawBuilding.productionLogic.powerNeeded;
+
+    if (hasKey(rawBuilding.productionLogic, "powerNeededForTenPeople")) {
+        return rawBuilding.productionLogic.powerNeededForTenPeople;
+    }
+
+    if (
+        !hasKey(rawBuilding.productionLogic, "productionDefinition") ||
+        !hasKey(rawBuilding.productionLogic.productionDefinition, "powerNeeded")
+    ) {
+        return 0;
+    }
+    return rawBuilding.productionLogic.productionDefinition.powerNeeded;
+}
+
+function getInputFromRawBuilding(rawBuilding: Building) {
+    const parsedInput: ParsedBuilding["input"] = {};
+
+    const consumables = getConsumablesOfRawBuilding(rawBuilding);
+    if (consumables.length === 0) return parsedInput;
+    if (typeof consumables[0] === "string") return parsedInput; // TODO: Decide what should be returned as input for storage houses
+    for (const consumable of consumables as ResourceQuantity[]) {
+        parsedInput[consumable.resourceName] = consumable.amount;
+    }
+    return parsedInput;
+}
+
+function getProducablesOfRawBuilding(rawBuilding: Building) {
+    if (
+        !hasKey(rawBuilding, "productionLogic") ||
+        !hasKey(rawBuilding.productionLogic, "productionDefinition")
+    )
+        return [];
+    return rawBuilding.productionLogic.productionDefinition.producables;
+}
+
+function getOutputFromRawBuilding(rawBuilding: Building) {
+    const outputs: ParsedBuilding["output"] = {};
+
+    const producables = getProducablesOfRawBuilding(rawBuilding);
+    for (const producable of producables) {
+        outputs[producable.resourceName] = producable.amount;
+    }
+    return outputs;
+}
+
+function getDurationFromRawBuilding(rawBuilding: Building) {
+    if (
+        !hasKey(rawBuilding, "productionLogic") ||
+        !hasKey(rawBuilding.productionLogic, "productionDefinition")
+    )
+        return 0;
+    return rawBuilding.productionLogic.productionDefinition.timeSteps / 5000;
+}
+
+function parseRawBuilding(buildingName: BuildingName, rawBuilding: Building) {
+    const categoryPath = getCategoryPath(parsedConstructionCategories, buildingName);
+    const duration = getDurationFromRawBuilding(rawBuilding);
+    const maxWorkers = getMaxWorkersFromRawBuilding(rawBuilding);
+    const parsedCosts = rawCostsToParsedCosts(rawBuilding);
+    const parsedInput = getInputFromRawBuilding(rawBuilding);
+    const parsedOutput = getOutputFromRawBuilding(rawBuilding);
+    const power = getPowerFromRawBuilding(rawBuilding);
+    return { categoryPath, duration, maxWorkers, parsedCosts, parsedInput, parsedOutput, power };
+}
+
+function parseBuilding(buildings: Buildings) {
+    const parsedBuildings: Record<string, ParsedBuilding> = {};
+
+    for (const [buildingName, rawBuilding] of entriesOf(buildings)) {
+        const {
+            categoryPath,
+            duration,
+            maxWorkers: workers,
+            parsedCosts: buildCost,
+            parsedInput: input,
+            parsedOutput: output,
+            power,
+        } = parseRawBuilding(buildingName, rawBuilding);
+
+        parsedBuildings[buildingName] = {
+            name: buildingName,
+            buildCost,
+            categoryPath,
+            duration,
+            input,
+            output,
+            power,
+            workers,
+        };
+    }
+    return parsedBuildings;
+}
+
+const parsedBuildings: Record<string, ParsedBuilding> = parseBuilding(buildings);
+console.log(parsedBuildings);
